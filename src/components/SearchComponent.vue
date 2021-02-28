0463deaf5638d2e826eda02de8ec3b1741db9e84
TakashiAihara <takashi.aihara.0523@gmail.com>
2021-02-28 18:12:06 +0900

    first commit

---
+++ src/components/SearchComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="search-component">
+    <h2>検索条件</h2>
+    <h3>User Name</h3>
+    <input type="text" v-model="username">
+    <input value="Search" type="button" @click="test">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  username = "";
+  // @Prop() private username!: string;
+  test(){
+    alert(this.username);
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <SearchComponent></SearchComponent>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import SearchComponent from '@/components/SearchComponent.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    SearchComponent,
+  },
+})
+export default class Home extends Vue {}
+</script>
