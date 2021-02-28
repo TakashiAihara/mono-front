bbdaf614dda91e86c6d297188b5c48b942f659ed
TakashiAihara <takashi.aihara.0523@gmail.com>
2021-02-28 18:39:50 +0900

    [36] result 仮実装

---
+++ src/components/SearchResult.vue
@@ -0,0 +1,60 @@
+<template v-if="data.length">
+  <div class="search-result">
+    <h2>検索結果</h2>
+    <table class="result-table">
+      <thead>
+        <tr>
+          <th>User Id</th>
+          <th>User Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, idx) in data" :key="idx">
+          <td>{{ item.id }}</td>
+          <td>{{ item.name }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  username = "";
+  @Prop() private data!: string;
+  onClick() {
+    alert(this.username);
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.result-table {
+  border: 1px solid black;
+  margin: auto;
+
+  th,
+  td {
+    border: 1px solid black;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <SearchComponent></SearchComponent>
+    <SearchResult :data="data"></SearchResult>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import SearchComponent from '@/components/SearchComponent.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
+import SearchComponent from "@/components/SearchComponent.vue"; // @ is an alias to /src
+import SearchResult from "@/components/SearchResult.vue"; // @ is an alias to /src
 
 @Component({
   components: {
     SearchComponent,
+    SearchResult
   },
 })
 export default class Home extends Vue {
